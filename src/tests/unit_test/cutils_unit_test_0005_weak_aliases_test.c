c77f34790bdea796bd9cb9713b7555ac7a54ffe2
Davit Kalantaryan <davit.kalantar@desy.de>
2023-07-23 20:46:28 +0200

    fixed typo

--- src/tests/unit_test/cutils_unit_test_0005_weak_aliases_test.c
+++ src/tests/unit_test/cutils_unit_test_0005_weak_aliases_test.c
@@ -25,16 +25,16 @@ CPPUTILS_UTEST_ALL(cutils_unit_test_0005_weak_aliases_test, t_0001) {
 CPPUTILS_BEGIN_C
 
 
-static void cutils_unit_test_0005_weak_aliases_test_clean(void) {
-    printf("s_nNumberOfFunctionCall=%d\n", s_nNumberOfFunctionCall);
-    //CinternalUnitTestAssertCheck(s_nNumberOfFunctionCall==2);
-}
-
-
-CPPUTILS_C_CODE_INITIALIZER(cutils_unit_test_0005_weak_aliases_test_init) {
-
-    //s_nNumberOfFunctionCall = 0;  // setting this 0, can be done after function call
-    atexit(&cutils_unit_test_0005_weak_aliases_test_clean);
-}
+//static void cutils_unit_test_0005_weak_aliases_test_clean(void) {
+//    printf("s_nNumberOfFunctionCall=%d\n", s_nNumberOfFunctionCall);
+//    //CinternalUnitTestAssertCheck(s_nNumberOfFunctionCall==2);
+//}
+//
+//
+//CPPUTILS_C_CODE_INITIALIZER(cutils_unit_test_0005_weak_aliases_test_init) {
+//
+//    //s_nNumberOfFunctionCall = 0;  // setting this 0, can be done after function call
+//    atexit(&cutils_unit_test_0005_weak_aliases_test_clean);
+//}
 
 CPPUTILS_END_C
