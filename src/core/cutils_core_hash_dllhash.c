b8bb8a7603d6f526c4f5691131b396201a198b7f
Davit Kalantaryan <davit.kalantar@desy.de>
2023-10-18 13:56:33 +0200

    recent changes in the cinternal concerning hashes was applied

--- src/core/cutils_core_hash_dllhash.c
+++ src/core/cutils_core_hash_dllhash.c
@@ -12,11 +12,19 @@
 #define CInternalDLListAddCreatedIteratorFrontInline1_needed	1
 #define	CInternalDLListAddCreatedIteratorFrontInline2_needed	1
 #include "cutils_core_list_dllist.impl.h"
+#define cinternal_hash1_small_int_needed
+#define CinternalIsMemoriesIdenticalSmallInt_needed
+#define CinternalStoreKeySmallInt_needed
+#define CinternalUnstoreKeySmallInt_needed
+#define CinternalStoreKeyRawMemory_needed
+#define CinternalUnstoreKeyRawMemory_needed
+#define CinternalIsMemoriesIdenticalRawMemory_needed
+//#define DefaultRemainingCleaner_needed
+#define cinternal_hash1_raw_mem_needed
+#include <cinternal/hash/functions.h>
 #include <string.h>
 
 #define CUTILS_HASH_DEFAULT_NUMBER_OF_BASKETS	4096
-
-static size_t cinternal_hash1_raw_mem(const void* a_pKey, size_t a_unKeySize) CPPUTILS_NOEXCEPT;
 
 
 CPPUTILS_BEGIN_C
@@ -37,64 +45,6 @@ struct CPPUTILS_DLL_PRIVATE SCinternalDLLHash {
 	TypeCinternalStoreKey				keyStore;
 	TypeCinternalUnstoreKey				keyUnstore;
 };
-
-
-static size_t cinternal_hash1_small_int(const void* a_key, size_t a_keySize) CPPUTILS_NOEXCEPT
-{
-	CPPUTILS_STATIC_CAST(void, a_keySize);
-	return CPPUTILS_REINTERPRET_CAST(size_t, a_key);
-}
-
-
-static bool CinternalIsMemoriesIdenticalRawMemory(const void* a_key1, size_t a_keySize1, const void* a_key2, size_t a_keySize2) CPPUTILS_NOEXCEPT
-{
-	return (a_keySize1 == a_keySize2) && (memcmp(a_key1, a_key2, a_keySize1) == 0);
-}
-
-
-static bool CinternalStoreKeyRawMemory(TypeCinternalAllocator a_allocator, void** a_pKeyStore, size_t* a_pKeySizeStore, const void* a_key, size_t a_keySize) CPPUTILS_NOEXCEPT
-{
-	*a_pKeyStore = (*a_allocator)(a_keySize);
-	if (!(*a_pKeyStore)) {
-		return false;
-	}
-	memcpy(*a_pKeyStore, a_key, a_keySize);
-	*a_pKeySizeStore = a_keySize;
-	return true;
-}
-
-
-static void CinternalUnstoreKeyRawMemory(TypeCinternalDeallocator a_deallocator, void* a_key, size_t a_keySize) CPPUTILS_NOEXCEPT
-{
-	CPPUTILS_STATIC_CAST(void, a_keySize);
-	(*a_deallocator)(a_key);
-}
-
-
-static bool CinternalIsMemoriesIdenticalSmallInt(const void* a_key1, size_t a_keySize1, const void* a_key2, size_t a_keySize2) CPPUTILS_NOEXCEPT
-{
-	CPPUTILS_STATIC_CAST(void, a_keySize1);
-	CPPUTILS_STATIC_CAST(void, a_keySize2);
-	return (a_key1 == a_key2);
-}
-
-
-static bool CinternalStoreKeySmallInt(TypeCinternalAllocator a_allocator, void** a_pKeyStore, size_t* a_pKeySizeStore, const void* a_key, size_t a_keySize) CPPUTILS_NOEXCEPT
-{
-	CPPUTILS_STATIC_CAST(void, a_allocator);
-	CPPUTILS_STATIC_CAST(void, a_keySize);
-	CPPUTILS_STATIC_CAST(void, a_pKeySizeStore);
-	*a_pKeyStore = CPPUTILS_CONST_CAST(void*,a_key);
-	return true;
-}
-
-
-static void CinternalUnstoreKeySmallInt(TypeCinternalDeallocator a_deallocator, void* a_key, size_t a_keySize) CPPUTILS_NOEXCEPT
-{
-	CPPUTILS_STATIC_CAST(void, a_deallocator);
-	CPPUTILS_STATIC_CAST(void, a_key);
-	CPPUTILS_STATIC_CAST(void, a_keySize);
-}
 
 
 CUTILS_EXPORT CinternalDLLHash_t CInternalDLLHashCreateExAny(size_t a_numberOfBaskets,
@@ -325,68 +275,3 @@ CUTILS_EXPORT void* CInternalLHashGetDefaultFunctions(int a_function)
 
 
 CPPUTILS_END_C
-
-
-
-
-/* The mixing step */
-#define mix2(a,b,c) \
-{ \
-  a=a-b;  a=a-c;  a=a^(c>>13); \
-  b=b-c;  b=b-a;  b=b^(a<<8);  \
-  c=c-a;  c=c-b;  c=c^(b>>13); \
-  a=a-b;  a=a-c;  a=a^(c>>12); \
-  b=b-c;  b=b-a;  b=b^(a<<16); \
-  c=c-a;  c=c-b;  c=c^(b>>5);  \
-  a=a-b;  a=a-c;  a=a^(c>>3);  \
-  b=b-c;  b=b-a;  b=b^(a<<10); \
-  c=c-a;  c=c-b;  c=c^(b>>15); \
-}
-
-static size_t cinternal_hash1_raw_mem(const void* a_pKey, size_t a_unKeySize) CPPUTILS_NOEXCEPT
-{
-	const uint32_t unKeySize = CPPUTILS_STATIC_CAST(uint32_t, a_unKeySize);
-	CPPUTILS_REGISTER const uint8_t* k = CPPUTILS_STATIC_CAST(const uint8_t*, a_pKey);
-	CPPUTILS_REGISTER uint32_t a, b, c;  /* the internal state */
-
-	uint32_t          len;    /* how many key bytes still need mixing */
-
-	/* Set up the internal state */
-	len = unKeySize;
-	a = b = 0x9e3779b9;  /* the golden ratio; an arbitrary value */
-	c = 13;         /* variable initialization of internal state */
-
-	/*---------------------------------------- handle most of the key */
-	while (len >= 12) {
-		a = a + (k[0] + (CPPUTILS_STATIC_CAST(uint32_t, k[1]) << 8) + (CPPUTILS_STATIC_CAST(uint32_t, k[2]) << 16) + (CPPUTILS_STATIC_CAST(uint32_t, k[3]) << 24));
-		b = b + (k[4] + (CPPUTILS_STATIC_CAST(uint32_t, k[5]) << 8) + (CPPUTILS_STATIC_CAST(uint32_t, k[6]) << 16) + (CPPUTILS_STATIC_CAST(uint32_t, k[7]) << 24));
-		c = c + (k[8] + (CPPUTILS_STATIC_CAST(uint32_t, k[9]) << 8) + (CPPUTILS_STATIC_CAST(uint32_t, k[10]) << 16) + (CPPUTILS_STATIC_CAST(uint32_t, k[11]) << 24));
-		mix2(a, b, c);
-		k = k + 12; len = len - 12;
-	}
-
-	/*------------------------------------- handle the last 11 bytes */
-	c = c + unKeySize;
-
-	/* all the case statements fall through */
-	switch (len) {
-	case 11: c = c + (CPPUTILS_STATIC_CAST(uint32_t, k[10]) << 24); CPPUTILS_FALLTHROUGH
-	case 10: c = c + (CPPUTILS_STATIC_CAST(uint32_t, k[9]) << 16); CPPUTILS_FALLTHROUGH
-	case 9: c = c + (CPPUTILS_STATIC_CAST(uint32_t, k[8]) << 8); CPPUTILS_FALLTHROUGH
-
-		/* the first byte of c is reserved for the length */
-	case 8: b = b + (CPPUTILS_STATIC_CAST(uint32_t, k[7]) << 24); CPPUTILS_FALLTHROUGH
-	case 7: b = b + (CPPUTILS_STATIC_CAST(uint32_t, k[6]) << 16); CPPUTILS_FALLTHROUGH
-	case 6: b = b + (CPPUTILS_STATIC_CAST(uint32_t, k[5]) << 8); CPPUTILS_FALLTHROUGH
-	case 5: b = b + k[4]; CPPUTILS_FALLTHROUGH
-	case 4: a = a + (CPPUTILS_STATIC_CAST(uint32_t, k[3]) << 24); CPPUTILS_FALLTHROUGH
-	case 3: a = a + (CPPUTILS_STATIC_CAST(uint32_t, k[2]) << 16); CPPUTILS_FALLTHROUGH
-	case 2: a = a + (CPPUTILS_STATIC_CAST(uint32_t, k[1]) << 8); CPPUTILS_FALLTHROUGH
-	case 1: a = a + k[0];
-		/* case 0: nothing left to add */
-	}
-	mix2(a, b, c);
-	/*-------------------------------------------- report the result */
-
-	return CPPUTILS_STATIC_CAST(size_t, c);
-}
